56e5a57824d4a51cc78b6a81c41e8f6bfc2d3b6a
Azrael <140682015+unhingedazrael@users.noreply.github.com>
2025-09-30 06:01:09 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,104 +1,107 @@
-#include <stdio.h>      
-#include <stdlib.h>     
-#include <string.h>     
-#include <unistd.h>     
-#include <arpa/inet.h>  
-#include <sys/types.h>  
-#include <sys/socket.h> 
-#include <netinet/in.h> 
 
-#define buffer_size 3000 // Define buffer size for reading data
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
 
-// Function to serve a requested file to the client
-int serve_file(int client_socket, char* file_path) {
-    FILE* fp = fopen(file_path, "rb"); // Open the requested file in binary read mode
-    char buffer[buffer_size];
-    int bytes_read;
+#define BUFFER_SIZE 3000  // buffer size
 
-    if (fp == NULL) {
-        // If the file doesn't exist, send a 404 Not Found HTTP response
-        const char* message = "HTTP/1.1 404 Not Found\r\nContent-Length: 13\r\n\r\n404 Not Found";
+void banana(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
+}
+
+int serve_file(int client_socket, const char* file_path) {
+    FILE* filepointer = fopen(file_path, "rb");
+    char buffer[BUFFER_SIZE];
+    size_t bytes_read;
+
+    if (!filepointer) {
+        const char* message =
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Length: 13\r\n\r\n"
+            "404 Not Found";
+
         send(client_socket, message, strlen(message), 0);
         return -1;
     }
 
-    // Send a 200 OK HTTP header to the client
-    const char *header = "HTTP/1.1 200 OK\r\n\r\n";
+    const char* header = "HTTP/1.1 200 OK\r\n\r\n";
     send(client_socket, header, strlen(header), 0);
 
-    // Read the file in chunks and send it to the client
-    while ((bytes_read = fread(buffer, 1, buffer_size, fp)) > 0) {
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, filepointer)) > 0) {
         send(client_socket, buffer, bytes_read, 0);
     }
 
-    fclose(fp); // Close the file
-    return 0;
+    fclose(filepointer);
+    return EXIT_SUCCESS;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        // Check if port number is provided
-        printf("Usage: %s <port>\n", argv[0]);
-        exit(-1);
+void* client_thread(void* arg) {
+    int client_socket = *(int*)arg;
+    free(arg);
+
+    char buffer[BUFFER_SIZE], method[8], path[256];
+
+    int bytes_received = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        if (sscanf(buffer, "%7s %255s", method, path) == 2) {
+            memmove(path, path + 1, strlen(path)); // strip leading '/'
+            serve_file(client_socket, path);
+        }
     }
 
-    char buffer[buffer_size];
-    int server_socket, client_socket;
+    close(client_socket);
+    return NULL;
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        banana("missing port");
+    }
+
+    int server_socket;
     struct sockaddr_in server_address, client_address;
     socklen_t client_len = sizeof(client_address);
-    char method[8], path[256]; // Buffers to hold HTTP method and path
 
-    // Create a TCP socket
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket < 0) {
-        perror("socket");
-        exit(-1);
-    }
+    if (server_socket < 0) banana("socket");
 
-    // Set up the server address struct
-    server_address.sin_family = AF_INET;                  // Use IPv4
-    server_address.sin_port = htons(atoi(argv[1]));       // Convert port to network byte order
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);   // Accept connections from any IP
+    int option = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
 
-    // Bind the socket to the specified port and IP
-    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-        perror("bind");
-        exit(-1);
-    }
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(atoi(argv[1]));
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // Start listening for incoming connections (up to 3 pending)
-    if (listen(server_socket, 3) < 0) {
-        perror("listen");
-        exit(-1);
-    }
+    if (bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0)
+        banana("bind");
 
-    printf("Server listening on %s\n", argv[1]);
+    if (listen(server_socket, 10) < 0)
+        banana("listen");
 
-    // Main server loop: accept and handle incoming connections
+    printf("Server listening on port %s\n", argv[1]);
+
     while (1) {
-        // Accept a new connection
-        client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_len);
-        if (client_socket < 0) {
-            perror("accept");
-            exit(-1);
-        }
+        int client_socket = accept(server_socket, (struct sockaddr*)&client_address, &client_len);
+        if (client_socket < 0) banana("accept");
 
-        // Receive the HTTP request
-        recv(client_socket, buffer, buffer_size, 0);
+        int *client_pointer = malloc(sizeof(int));
+        *client_pointer = client_socket;
 
-        // Parse the HTTP method and path (e.g., "GET /index.html")
-        sscanf(buffer, "%s %s", method, path);
-
-        // Remove the leading '/' from the path to form a valid filename
-        memmove(path, path + 1, strlen(path));
-
-        // Attempt to serve the requested file
-        serve_file(client_socket, path);
-
-        // Close the connection with the client
-        close(client_socket);
+        pthread_t tid;
+        pthread_create(&tid, NULL, client_thread, client_pointer);
+        pthread_detach(tid);
     }
 
-    // Close the server socket 
     close(server_socket);
+    return EXIT_SUCCESS;
 }
